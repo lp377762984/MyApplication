f8d75031e53460c8c224c8caa565c915fe38d38b
孙宏宇 <644563767@qq.com>
2017-09-20 17:56:24 +0800

    111

--- Android/dl-project/app/src/main/java/com/cn/danceland/danceland/MainActivity.java
+++ Android/dl-project/app/src/main/java/com/cn/danceland/danceland/MainActivity.java
@@ -14,4 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
 }
