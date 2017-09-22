b0983b168427cb9b4e576d8973c2799125c0c6e8
zzf <410464149@qq.com>
2017-09-22 15:15:42 +0800

    112

--- Android/MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/MainActivity.java
+++ Android/MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/MainActivity.java
@@ -1,13 +1,27 @@
 package com.cn.danceland.myapplication;
 
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+//        tabLayout= (TabLayout) findViewById(R.id.tablayout);
+//
+//        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+//
+//        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+//
+//        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+//        tabLayout.addTab(tabLayout.newTab().setText("Tab 4"));
+//        tabLayout.addTab(tabLayout.newTab().setText("Tab 5"));
+//        tabLayout.addTab(tabLayout.newTab().setText("Tab 6"));
+//        tabLayout.addTab(tabLayout.newTab().setText("Tab 7"));
+//        tabLayout.addTab(tabLayout.newTab().setText("Tab 8"));
+        //tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
     }
 }
