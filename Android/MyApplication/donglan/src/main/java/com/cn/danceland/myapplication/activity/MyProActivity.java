5eefde90c06ecc3f75b72fb27ab57b77d1ef1c80
zzf <410464149@qq.com>
2017-11-14 17:26:02 +0800

    Merge branch 'master' of 192.168.1.98:/home/git/git_root/projects/dl-mobile
    # Conflicts:
    #	Android/MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/activity/SettingActivity.java

--- Android/MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/activity/MyProActivity.java
+++ Android/MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/activity/MyProActivity.java
@@ -10,10 +10,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -26,13 +29,27 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.bumptech.glide.Glide;
+import com.cn.danceland.myapplication.MyApplication;
 import com.cn.danceland.myapplication.R;
+import com.cn.danceland.myapplication.utils.Constants;
+import com.cn.danceland.myapplication.utils.LogUtil;
 import com.cn.danceland.myapplication.utils.PictureUtil;
+import com.cn.danceland.myapplication.utils.multipartrequest.MultipartRequest;
+import com.cn.danceland.myapplication.utils.multipartrequest.MultipartRequestParams;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URI;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.R.attr.bitmap;
 
 /**
  * Created by feng on 2017/10/10.
@@ -46,7 +63,7 @@ public class MyProActivity extends Activity {
     View headView,rootview,sexView;
     ImageView back;
     int flag;
-    Uri uri;
+    Uri uri,mCutUri;
     ContentResolver resolver;
     public final static int ALBUM_REQUEST_CODE = 1;
     public final static int CROP_REQUEST = 2;
@@ -219,7 +236,26 @@ public class MyProActivity extends Activity {
     }
 
     public void photoGraph (){
-
+//        File outputfile = new File(MyProActivity.this.getExternalCacheDir(),System.currentTimeMillis() + ".png");
+//        try {
+//            if (outputfile.exists()){
+//                outputfile.delete();//删除
+//            }
+//            outputfile.createNewFile();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        Uri imageuri ;
+//        if (Build.VERSION.SDK_INT >= 24){
+//            imageuri = FileProvider.getUriForFile(MyProActivity.this,
+//                    "com.cn.danceland.myapplication", //可以是任意字符串
+//                    outputfile);
+//        }else{
+//            imageuri = Uri.fromFile(outputfile);
+//        }
+//        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+//        intent.putExtra(MediaStore.EXTRA_OUTPUT,imageuri);
+//        startActivityForResult(intent,CAMERA_REQUEST_CODE);
         // 指定相机拍摄照片保存地址
         String state = Environment.getExternalStorageState();
         if (state.equals(Environment.MEDIA_MOUNTED)) {
@@ -236,7 +272,8 @@ public class MyProActivity extends Activity {
             // 设置系统相机拍摄照片完成后图片文件的存放地址
             intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
             startActivityForResult(intent, CAMERA_REQUEST_CODE);
-        } else {
+        }
+        else {
             Toast.makeText(getApplicationContext(), "请确认已经插入SD卡",
                     Toast.LENGTH_LONG).show();
         }
@@ -246,19 +283,53 @@ public class MyProActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == Activity.RESULT_OK) {
+            MultipartRequestParams params = new MultipartRequestParams();
             if (requestCode == CAMERA_REQUEST_CODE) {
-                circleImageView.setImageBitmap(PictureUtil.getSmallBitmap(cameraPath,65,65));
+                startPhotoZoom(uri);
             }
             if(requestCode == ALBUM_REQUEST_CODE){
-                Uri photo = data.getData();
-                String path;
-                if(photo!=null&&photo.toString().contains("file://")){
-                    path = photo.toString().replace("file://","");
-                }else{
-                    path = PictureUtil.getRealPath(this,photo);
-                }
-                circleImageView.setImageBitmap(PictureUtil.getSmallBitmap(path,65,65));
+                startActivityForResult(CutForPhoto(data.getData()),10010);
             }
+            if(requestCode==10010){
+                Glide.with(MyProActivity.this).load(mCutUri).into(circleImageView);
+            }
+            if(requestCode==222){
+                Bundle extras = data.getExtras();
+                if (extras != null) {
+                    Bitmap photo = extras.getParcelable("data");
+                    File fileCut = new File(SAVED_IMAGE_DIR_PATH + System.currentTimeMillis() + ".png");
+                    try{
+                        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(fileCut));
+                        photo.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+                        bos.flush();
+                        bos.close();
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                    Drawable drawable = new BitmapDrawable(getResources(), photo);
+                    Glide.with(MyProActivity.this).load(drawable).into(circleImageView);
+                }
+            }
+
+            MultipartRequest request = new MultipartRequest(Request.Method.POST, params, Constants.UPLOADFILE_URL, new Response.Listener<String>() {
+
+                @Override
+                public void onResponse(String s) {
+
+                    LogUtil.e("zzf",s);
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError volleyError) {
+                    LogUtil.e("zzf",volleyError.toString());
+                }
+            }
+            ) {
+            };
+
+      //      MyApplication.getHttpQueues().add(request);
+
         }
 
     }
@@ -272,4 +343,115 @@ public class MyProActivity extends Activity {
 
     }
 
+    private Intent CutForPhoto(Uri uri) {
+        try {
+            //直接裁剪
+            Intent intent = new Intent("com.android.camera.action.CROP");
+            //设置裁剪之后的图片路径文件
+            File cutfile = new File(Environment.getExternalStorageDirectory().getPath(),
+                    System.currentTimeMillis()+".png"); //随便命名一个
+            if (cutfile.exists()){ //如果已经存在，则先删除,这里应该是上传到服务器，然后再删除本地的，没服务器，只能这样了
+                cutfile.delete();
+            }
+            cutfile.createNewFile();
+            //初始化 uri
+            Uri imageUri = uri; //返回来的 uri
+            Uri outputUri = null; //真实的 uri
+            //Log.d(TAG, "CutForPhoto: "+cutfile);
+            outputUri = Uri.fromFile(cutfile);
+            mCutUri = outputUri;
+            //Log.d(TAG, "mCameraUri: "+mCutUri);
+            // crop为true是设置在开启的intent中设置显示的view可以剪裁
+            intent.putExtra("crop",true);
+            // aspectX,aspectY 是宽高的比例，这里设置正方形
+            intent.putExtra("aspectX",1);
+            intent.putExtra("aspectY",1);
+            //设置要裁剪的宽高
+            intent.putExtra("outputX", 200); //200dp
+            intent.putExtra("outputY",200);
+            intent.putExtra("scale",true);
+            //如果图片过大，会导致oom，这里设置为false
+            intent.putExtra("return-data",false);
+            if (imageUri != null) {
+                intent.setDataAndType(imageUri, "image/*");
+            }
+            if (outputUri != null) {
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, outputUri);
+            }
+            intent.putExtra("noFaceDetection", true);
+            //压缩图片
+            intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+            return intent;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private Intent CutForCamera(String camerapath,String imgname) {
+        try {
+
+            //设置裁剪之后的图片路径文件
+            File cutfile = new File(Environment.getExternalStorageDirectory().getPath(),
+                    System.currentTimeMillis() + ".png"); //随便命名一个
+            if (cutfile.exists()){ //如果已经存在，则先删除,这里应该是上传到服务器，然后再删除本地的，没服务器，只能这样了
+                cutfile.delete();
+            }
+            cutfile.createNewFile();
+            //初始化 uri
+            Uri imageUri = null; //返回来的 uri
+            Uri outputUri = null; //真实的 uri
+            Intent intent = new Intent("com.android.camera.action.CROP");
+            //拍照留下的图片
+            File camerafile = new File(camerapath,imgname);
+            if (Build.VERSION.SDK_INT >= 24) {
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                imageUri = FileProvider.getUriForFile(MyProActivity.this,
+                        "com.cn.danceland.myapplication",
+                        camerafile);
+            } else {
+                imageUri = Uri.fromFile(camerafile);
+            }
+            outputUri = Uri.fromFile(cutfile);
+            //把这个 uri 提供出去，就可以解析成 bitmap了
+            mCutUri = outputUri;
+            // crop为true是设置在开启的intent中设置显示的view可以剪裁
+            intent.putExtra("crop",true);
+            // aspectX,aspectY 是宽高的比例，这里设置正方形
+            intent.putExtra("aspectX",1);
+            intent.putExtra("aspectY",1);
+            //设置要裁剪的宽高
+            intent.putExtra("outputX", 200);
+            intent.putExtra("outputY",200);
+            intent.putExtra("scale",true);
+            //如果图片过大，会导致oom，这里设置为false
+            intent.putExtra("return-data",false);
+            if (imageUri != null) {
+                intent.setDataAndType(imageUri, "image/*");
+            }
+            if (outputUri != null) {
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, outputUri);
+            }
+            intent.putExtra("noFaceDetection", true);
+            //压缩图片
+            intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+            return intent;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void startPhotoZoom(Uri uri) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        intent.putExtra("crop", true);
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("outputX", 300);
+        intent.putExtra("outputY", 300);
+        intent.putExtra("return-data", true);
+        intent.putExtra("noFaceDetection", true);
+        startActivityForResult(intent, 222);
+    }
 }
