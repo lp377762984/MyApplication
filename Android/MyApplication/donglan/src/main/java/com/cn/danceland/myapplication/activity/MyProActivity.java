f8d19fd43301f1956337003dbed02d3d5b421a58
zzf <410464149@qq.com>
2017-11-16 13:10:11 +0800

    Merge branch 'master' of 192.168.1.98:/home/git/git_root/projects/dl-mobile
    # Conflicts:
    #	Android/MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/activity/SettingActivity.java

--- Android/MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/activity/MyProActivity.java
+++ Android/MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/activity/MyProActivity.java
@@ -293,6 +293,42 @@ public class MyProActivity extends Activity {
         queue.add(stringRequest);
     }
 
+    public void commitSelf(){
+
+        StringRequest stringRequest = new StringRequest(Request.Method.PUT,Constants.MODIFYY_IMAGE , new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                if(s.contains("true")){
+                    ToastUtils.showToastShort("修改成功！");
+                }else{
+                    ToastUtils.showToastShort("修改失败！");
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                ToastUtils.showToastShort("修改失败！请检查网络");
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String,String> map = new HashMap<String,String>();
+                map.put("self_Avatar_path",selfAvatarPath);
+                return map;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String,String> map = new HashMap<String,String>();
+                map.put("Authorization", SPUtils.getString(Constants.MY_TOKEN,""));
+
+                return map;
+            }
+        };
+        queue.add(stringRequest);
+    }
+
+
     public void commitSex(){
 
 
@@ -382,7 +418,7 @@ public class MyProActivity extends Activity {
                         infoData.setSelfAvatarPath(selfAvatarPath);
                         //发送事件
                         EventBus.getDefault().post(new StringEvent(selfAvatarPath,99));
-                        LogUtil.e("zzf",selfAvatarPath);
+                        commitSelf();
                         DataInfoCache.saveOneCache(infoData,Constants.MY_INFO);
                         //LogUtil.e("zzf",s);
                     }
@@ -400,6 +436,8 @@ public class MyProActivity extends Activity {
         }
 
     }
+
+
 
     public void  photoAlbum(){
 
@@ -448,60 +486,6 @@ public class MyProActivity extends Activity {
             intent.putExtra("noFaceDetection", true);
 //            //压缩图片
             intent.putExtra("outputFormat", Bitmap.CompressFormat.PNG.toString());
-            return intent;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private Intent CutForCamera(String camerapath,String imgname) {
-        try {
-
-            //设置裁剪之后的图片路径文件
-            File cutfile = new File(Environment.getExternalStorageDirectory().getPath(),
-                    System.currentTimeMillis() + ".jpg"); //随便命名一个
-            if (cutfile.exists()){ //如果已经存在，则先删除,这里应该是上传到服务器，然后再删除本地的，没服务器，只能这样了
-                cutfile.delete();
-            }
-            cutfile.createNewFile();
-            //初始化 uri
-            Uri imageUri = null; //返回来的 uri
-            Uri outputUri = null; //真实的 uri
-            Intent intent = new Intent("com.android.camera.action.CROP");
-            //拍照留下的图片
-            File camerafile = new File(camerapath,imgname);
-            if (Build.VERSION.SDK_INT >= 24) {
-                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                imageUri = FileProvider.getUriForFile(MyProActivity.this,
-                        "com.cn.danceland.myapplication",
-                        camerafile);
-            } else {
-                imageUri = Uri.fromFile(camerafile);
-            }
-            outputUri = Uri.fromFile(cutfile);
-            //把这个 uri 提供出去，就可以解析成 bitmap了
-            mCutUri = outputUri;
-            // crop为true是设置在开启的intent中设置显示的view可以剪裁
-            intent.putExtra("crop",true);
-            // aspectX,aspectY 是宽高的比例，这里设置正方形
-            intent.putExtra("aspectX",1);
-            intent.putExtra("aspectY",1);
-            //设置要裁剪的宽高
-            intent.putExtra("outputX", 200);
-            intent.putExtra("outputY",200);
-            intent.putExtra("scale",true);
-            //如果图片过大，会导致oom，这里设置为false
-            intent.putExtra("return-data",false);
-            if (imageUri != null) {
-                intent.setDataAndType(imageUri, "image/*");
-            }
-            if (outputUri != null) {
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, outputUri);
-            }
-            intent.putExtra("noFaceDetection", true);
-            //压缩图片
-            intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
             return intent;
         } catch (IOException e) {
             e.printStackTrace();
