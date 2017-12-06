01ff8a3e9274fc0e058117b21101388c4cbe7c4b
zzf <410464149@qq.com>
2017-12-06 15:42:25 +0800

    1111

--- MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/fragment/ShopFragment.java
+++ MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/fragment/ShopFragment.java
@@ -340,7 +340,7 @@ public class ShopFragment extends BaseFragment {
                     AlertDialog.Builder dialog =
                             new AlertDialog.Builder(mActivity);
                     dialog.setTitle("提示");
-                    dialog.setMessage("是否关注此门店");
+                    dialog.setMessage("是否加入此门店");
                     dialog.setPositiveButton("确认", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
