141472c37c400326a65a9e7c0ad7a1a1fe4180bb
zzf <410464149@qq.com>
2018-01-18 11:15:19 +0800

    Merge branch 'master' of http://192.168.1.98:9090/root/dl-mobile-android
    # Conflicts:
    #	MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/utils/Constants.java

--- MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/fragment/CommentFragment.java
+++ MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/fragment/CommentFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.cn.danceland.myapplication.R;
 import com.cn.danceland.myapplication.adapter.MessageAdapter;
@@ -24,10 +25,12 @@ public class CommentFragment extends BaseFragment {
     List<MiMessage> messageList;
     Bundle arguments;
     String type;
+    TextView tv_no;
     @Override
     public View initViews() {
         View v = View.inflate(mActivity, R.layout.fragment_comment,null);
         lv_message = v.findViewById(R.id.lv_message);
+        tv_no = v.findViewById(R.id.tv_no);
         arguments = getArguments();
         initData();
         return v;
@@ -39,8 +42,12 @@ public class CommentFragment extends BaseFragment {
         //3表示的是评论类型的推送消息
         messageList = new ArrayList<MiMessage>();
         messageList = dbData.getMessageList(type);
-        if(messageList!=null){
+        if(messageList!=null&&messageList.size()>0){
+            tv_no.setVisibility(View.GONE);
             lv_message.setAdapter(new MessageAdapter(messageList,mActivity));
+        }else{
+            tv_no.setVisibility(View.VISIBLE);
+            lv_message.setVisibility(View.GONE);
         }
     }
 
