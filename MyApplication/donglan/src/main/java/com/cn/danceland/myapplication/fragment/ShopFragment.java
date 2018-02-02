9e59ea60eaac608cc436f5c9e892e2e923ed5760
zzf <410464149@qq.com>
2018-02-02 15:04:44 +0800

    z字体颜色

--- MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/fragment/ShopFragment.java
+++ MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/fragment/ShopFragment.java
@@ -86,8 +86,8 @@ public class ShopFragment extends BaseFragment {
         ibtn_gps = v.findViewById(R.id.ibtn_gps);
 
         ll_top = v.findViewById(R.id.ll_top);
-        ll_top.setBackgroundColor(Color.WHITE);
-        ll_top.getBackground().setAlpha(80);
+        ll_top.setBackgroundColor(Color.BLACK);
+        ll_top.getBackground().setAlpha(88);
         tv_shopname = v.findViewById(R.id.tv_shopname);
 
         mGridView.setOnItemClickListener(new MyOnItemClickListener());
