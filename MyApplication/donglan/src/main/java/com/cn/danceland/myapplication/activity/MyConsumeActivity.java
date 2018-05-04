db139f02dacf1b213984fd92e61fdada7a761ca1
zzf <410464149@qq.com>
2018-05-04 14:39:52 +0800

    Merge branch 'master' of http://192.168.1.98:9090/root/dl-mobile-android
    # Conflicts:
    #	MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/utils/Constants.java

--- MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/activity/MyConsumeActivity.java
+++ MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/activity/MyConsumeActivity.java
@@ -1,12 +1,29 @@
 package com.cn.danceland.myapplication.activity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.widget.ListView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.cn.danceland.myapplication.MyApplication;
 import com.cn.danceland.myapplication.R;
+import com.cn.danceland.myapplication.bean.MyConsumeCon;
+import com.cn.danceland.myapplication.utils.Constants;
+import com.cn.danceland.myapplication.utils.LogUtil;
+import com.cn.danceland.myapplication.utils.SPUtils;
 import com.cn.danceland.myapplication.view.DongLanTitleView;
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by feng on 2018/5/2.
@@ -16,6 +33,10 @@ public class MyConsumeActivity extends Activity {
 
     DongLanTitleView consume_title;
     ListView lv_consume;
+    Gson gson;
+    ProgressDialog progressDialog;
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,10 +54,46 @@ public class MyConsumeActivity extends Activity {
         consume_title.setTitle("我的消费");
         lv_consume = findViewById(R.id.lv_consume);
 
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("加载中...");
+        progressDialog.show();
+        initData();
+    }
+
+    private void initData() {
+        MyConsumeCon myConsumeCon = new MyConsumeCon();
+        myConsumeCon.setPage(0);
+        myConsumeCon.setSize(20);
+        String s = gson.toJson(myConsumeCon);
+
+        //progressDialog.show();
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, Constants.MYCONSUME, s,new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject jsonObject) {
+                progressDialog.dismiss();
+                LogUtil.i(jsonObject.toString());
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                LogUtil.i(volleyError.toString());
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> map = new HashMap<String, String>();
+                map.put("Authorization", SPUtils.getString(Constants.MY_TOKEN, ""));
+                return map;
+            }
+
+        };
+
+        MyApplication.getHttpQueues().add(jsonObjectRequest);
+
     }
 
     private void initHost() {
-
-
+        gson = new Gson();
     }
 }
