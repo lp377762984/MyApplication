0ffc0df225e1bf1f7d6bc2509be5fbe678f065fe
孙宏宇 <644563767@qq.com>
2018-06-13 09:39:42 +0800

    修复诺干bug

--- MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/utils/Constants.java
+++ MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/utils/Constants.java
@@ -20,7 +20,7 @@ public class Constants  {
     // public static final String HOST = "http://192.168.1.130:8003/";//超哥服务器地址
     //public static final String HOST = "http://coder-hc.oicp.io:57068/";//佳楠
     //public static String HOST = "http://192.168.1.117:8003/";//佳楠
-    public static final String HOST = " http://192.168.1.123:8003/";//
+    public static final String HOST = " http://192.168.1.117:8003/";//
    //  public static final String HOST = " http://192.168.1.96:8003/";//
   //  public static final String HOST = "http://192.168.1.123:8003/";//老高
      //public static  String HOST = "http://192.168.1.122:8003/";//超哥
