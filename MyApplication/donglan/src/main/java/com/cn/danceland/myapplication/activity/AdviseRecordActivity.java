d82867a209ea43cc22f82cbbb081e487afc3d55c
杨潇雪 <272782320@qq.com>
2018-11-23 17:55:50 +0800

    2.0 健身日记微调 意见反馈下拉刷新 登陆验证

--- MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/activity/AdviseRecordActivity.java
+++ MyApplication/donglan/src/main/java/com/cn/danceland/myapplication/activity/AdviseRecordActivity.java
@@ -1,13 +1,17 @@
 package com.cn.danceland.myapplication.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -17,20 +21,29 @@ import com.bumptech.glide.Glide;
 import com.cn.danceland.myapplication.R;
 import com.cn.danceland.myapplication.bean.DLResult;
 import com.cn.danceland.myapplication.bean.Data;
+import com.cn.danceland.myapplication.bean.RequsetDynInfoBean;
 import com.cn.danceland.myapplication.bean.feedback.FeedBack;
 import com.cn.danceland.myapplication.bean.feedback.FeedBackCond;
 import com.cn.danceland.myapplication.bean.feedback.FeedBackRequest;
 import com.cn.danceland.myapplication.utils.Constants;
 import com.cn.danceland.myapplication.utils.DataInfoCache;
+import com.cn.danceland.myapplication.utils.DensityUtils;
+import com.cn.danceland.myapplication.utils.LogUtil;
 import com.cn.danceland.myapplication.utils.TimeUtils;
 import com.cn.danceland.myapplication.utils.ToastUtils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import com.handmark.pulltorefresh.library.ILoadingLayout;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by feng on 2018/3/26.
@@ -38,7 +51,7 @@ import java.util.List;
 
 public class AdviseRecordActivity extends BaseActivity {
 
-    ListView advise_record_lv;
+    PullToRefreshListView pullToRefresh;
     private FeedBackRequest request;
     private Gson gson;
     Data data;
@@ -46,12 +59,13 @@ public class AdviseRecordActivity extends BaseActivity {
     RelativeLayout rl_error;
     ImageView iv_error;
     TextView tv_error;
+    ProgressDialog dialog;
+    RecordAdapter adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.advise_record);
-
 
         data = (Data) DataInfoCache.loadOneCache(Constants.MY_INFO);
         request = new FeedBackRequest();
@@ -63,22 +77,121 @@ public class AdviseRecordActivity extends BaseActivity {
 
     private void initView() {
 
-        advise_record_lv = findViewById(R.id.advise_record_lv);
-        advise_record_lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        pullToRefresh = findViewById(R.id.pullToRefresh);
+        View listEmptyView = findViewById(R.id.rl_no_info);
+        pullToRefresh.getRefreshableView().setEmptyView(listEmptyView);
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("正在加载……");
+        pullToRefresh.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 startActivity(new Intent(AdviseRecordActivity.this, AdviseDetailActivity.class).putExtra("id", list.get(position).getId()));
             }
         });
-        rl_error = findViewById(R.id.rl_error);
-        iv_error = rl_error.findViewById(R.id.iv_error);
-        Glide.with(this).load(R.drawable.img_error4).into(iv_error);
-        tv_error = rl_error.findViewById(R.id.tv_error);
-        tv_error.setText("您还没有反馈任何信息");
+//        rl_error = findViewById(R.id.rl_error);
+//        iv_error = rl_error.findViewById(R.id.iv_error);
+//        Glide.with(this).load(R.drawable.img_error4).into(iv_error);
+//        tv_error = rl_error.findViewById(R.id.tv_error);
+//        tv_error.setText("您还没有反馈任何信息");
+//        pullToRefresh.getRefreshableView().setEmptyView(tv_error);
 
-        advise_record_lv.setEmptyView(rl_error);
+        list=new ArrayList<>();
+        adapter = new RecordAdapter(list);
+        pullToRefresh.setAdapter(adapter);
+//        pullToRefresh.setEmptyView(rl_error);
+        //加入头布局
+        //      pullToRefresh.getRefreshableView().addHeaderView(initHeadview(userInfo));
+        pullToRefresh.getRefreshableView().setOverScrollMode(View.OVER_SCROLL_NEVER);//去掉下拉阴影
+        //设置下拉刷新模式both是支持下拉和上拉
+        pullToRefresh.setMode(PullToRefreshBase.Mode.PULL_FROM_START);
+        pullToRefresh.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2<ListView>() {
+            @Override
+            public void onPullDownToRefresh(
+                    PullToRefreshBase<ListView> refreshView) {
+
+                TimerTask task = new TimerTask() {
+                    public void run() {
+                        new DownRefresh().execute();
+                    }
+                };
+                Timer timer = new Timer();
+                timer.schedule(task, 1000);
+            }
+
+            @Override
+            public void onPullUpToRefresh(
+                    PullToRefreshBase<ListView> refreshView) {
+                TimerTask task = new TimerTask() {
+                    public void run() {
+                        new UpRefresh().execute();
+                    }
+                };
+                Timer timer = new Timer();
+                timer.schedule(task, 1000);
+            }
+        });
     }
 
+    /**
+     * 下拉刷新
+     */
+    private class DownRefresh extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            init();
+            list = new ArrayList<>();
+            queryList();
+            dialog.dismiss();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            dialog.dismiss();
+            adapter.notifyDataSetChanged();
+            pullToRefresh.onRefreshComplete();
+        }
+    }
+
+    /**
+     * 上拉拉刷新
+     */
+    private class UpRefresh extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+//            try {
+//                if (!isEnd) {//还有数据请求
+//                    findSelfDT();
+//                }
+//                dialog.dismiss();
+//
+//            } catch (Exception e) {
+//            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            pullToRefresh.onRefreshComplete();
+        }
+    }
+
+    private void init() {
+        // 设置下拉刷新文本
+        ILoadingLayout startLabels = pullToRefresh
+                .getLoadingLayoutProxy(true, false);
+        startLabels.setPullLabel("下拉刷新...");// 刚下拉时，显示的提示
+        startLabels.setRefreshingLabel("正在加载...");// 刷新时
+        startLabels.setReleaseLabel("放开刷新...");// 下来达到一定距离时，显示的提示
+        // 设置上拉刷新文本
+        ILoadingLayout endLabels = pullToRefresh.getLoadingLayoutProxy(
+                false, true);
+        endLabels.setPullLabel("上拉加载...");// 刚下拉时，显示的提示
+        endLabels.setRefreshingLabel("正在加载...");// 刷新时
+        endLabels.setReleaseLabel("放开刷新...");// 下来达到一定距离时，显示的提示
+    }
 
     /**
      * @方法说明:按条件查询意见反馈列表
@@ -89,12 +202,14 @@ public class AdviseRecordActivity extends BaseActivity {
         cond.setSize(20);
         request.queryList(cond, new Response.Listener<JSONObject>() {
             public void onResponse(JSONObject json) {
+                LogUtil.i(json.toString());
                 DLResult<List<FeedBack>> result = gson.fromJson(json.toString(), new TypeToken<DLResult<List<FeedBack>>>() {
                 }.getType());
                 if (result.isSuccess()) {
                     list = result.getData();
                     if (list != null) {
-                        advise_record_lv.setAdapter(new RecordAdapter(list));
+                        adapter.setList(list);
+                        adapter.notifyDataSetChanged();
                     }
                 } else {
                     ToastUtils.showToastShort("查询分页列表失败,请检查手机网络！");
@@ -114,6 +229,13 @@ public class AdviseRecordActivity extends BaseActivity {
 
         }
 
+        public void setList(List<FeedBack> list) {
+            this.list = list;
+        }
+
+        public void addLastList(List<FeedBack> lists) {
+            list.addAll(lists);
+        }
         @Override
         public int getCount() {
             return list.size();
@@ -140,11 +262,20 @@ public class AdviseRecordActivity extends BaseActivity {
                 viewHolder.fankui_time = convertView.findViewById(R.id.fankui_time);
                 viewHolder.huifu_time = convertView.findViewById(R.id.huifu_time);
                 viewHolder.tv_status = convertView.findViewById(R.id.tv_status);
+                viewHolder.item_layout_cv = convertView.findViewById(R.id.item_layout_cv);
                 convertView.setTag(viewHolder);
             } else {
                 viewHolder = (ViewHolder) convertView.getTag();
             }
-
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, DensityUtils.dp2px(AdviseRecordActivity.this, 60f));
+            if (position == 0) {
+                layoutParams.setMargins(DensityUtils.dp2px(AdviseRecordActivity.this, 16f), DensityUtils.dp2px(AdviseRecordActivity.this, 16f), DensityUtils.dp2px(AdviseRecordActivity.this, 16f), DensityUtils.dp2px(AdviseRecordActivity.this, 11f));
+            } else if (position == list.size() - 1) {
+                layoutParams.setMargins(DensityUtils.dp2px(AdviseRecordActivity.this, 16f), DensityUtils.dp2px(AdviseRecordActivity.this, 5f), DensityUtils.dp2px(AdviseRecordActivity.this, 16f), DensityUtils.dp2px(AdviseRecordActivity.this, 16f));
+            } else {
+                layoutParams.setMargins(DensityUtils.dp2px(AdviseRecordActivity.this, 16f), DensityUtils.dp2px(AdviseRecordActivity.this, 5f), DensityUtils.dp2px(AdviseRecordActivity.this, 16f), DensityUtils.dp2px(AdviseRecordActivity.this, 11f));
+            }
+            viewHolder.item_layout_cv.setLayoutParams(layoutParams);
             if (list.get(position).getType() == 1) {
                 viewHolder.tv_type.setText("批评");
                 viewHolder.type_iv.setImageDrawable(getResources().getDrawable(R.drawable.item_criticism_icon));
@@ -190,6 +321,6 @@ public class AdviseRecordActivity extends BaseActivity {
     class ViewHolder {
         ImageView type_iv;
         TextView tv_type, fankui_time, huifu_time, tv_status;
-
+        CardView item_layout_cv;
     }
 }
